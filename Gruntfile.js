bef97d46645652ecbefd33c8c4bbb5fb4fb9c8d9
Luc Hoang Long <d@dapps.me>
2015-04-10 19:21:08 +0700

    first version

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		jscs: {
+			src: [
+				'examples/**/*.js',
+				'lib/**/*.js',
+				'Gruntfile.js'
+			],
+			options: {
+				config: 'rules/.jscsrc'
+			}
+		},
+		jshint: {
+			src: '<%= jscs.src %>',
+			options: {
+				jshintrc: 'rules/.jshintrc-server',
+			}
+		}
+	});
+
+	// load all plugins
+	require('load-grunt-tasks')(grunt);
+
+	grunt.registerTask('default', ['jscs', 'jshint']);
+};
---
+++ examples/app.js
@@ -0,0 +1,93 @@
+'use strict';
+
+var engine = require('ect');
+var express = require('express');
+var path = require('path');
+var passport = require('passport');
+var session = require('express-session');
+
+var GoogleOauthJWTStrategy = require('../lib/passport-google-oauth-jwt').GoogleOauthJWTStrategy;
+
+// API Access link for creating client ID and secret:
+// https://code.google.com/apis/console/
+var GOOGLE_CLIENT_ID = '19650001560-msm738c7mjme3onf25vfu5u83ctuvoaa.apps.googleusercontent.com';
+var GOOGLE_CLIENT_SECRET = 'Y1p5u8rKR6O-cNvVJLk1yM5i';
+
+// config passport
+passport.serializeUser(function serialize(user, done) {
+	done(null, user);
+});
+
+passport.deserializeUser(function deserialize(obj, done) {
+	done(null, obj);
+});
+
+passport.use(new GoogleOauthJWTStrategy({
+	clientId: GOOGLE_CLIENT_ID,
+	clientSecret: GOOGLE_CLIENT_SECRET
+}, function verfiy(token, info, refreshToken, done) {
+	done(null, {
+		email: info.email
+	});
+}));
+
+// config express
+var app = express();
+
+var ect = engine({
+	root: path.resolve(__dirname, 'views')
+});
+
+app.engine('ect', ect.render);
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('view engine', 'ect');
+
+app.use(session({
+	secret: 'google-oauth-jwt'
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// route
+app.get('/', function(req, res, next) {
+	if (req.isAuthenticated()) {
+		return next();
+	}
+
+	res.redirect('/login');
+}, function(req, res) {
+	res.render('index', {
+		user: req.user
+	});
+});
+
+app.get('/login', function(req, res) {
+	res.render('login');
+});
+
+app.get('/logout', function(req, res) {
+	req.logout();
+	res.redirect('/');
+});
+
+// request google login
+app.get('/auth/google', passport.authenticate('google-oauth-jwt', {
+	callbackUrl: 'http://localhost:3000/auth/google/callback',
+	scope: [
+		'email'
+	]
+}), function(req, res) {
+	res.redirect('/');
+});
+
+// handle google callback
+app.get('/auth/google/callback', passport.authenticate('google-oauth-jwt', {
+	callbackUrl: 'http://localhost:3000/auth/google/callback'
+}), function(req, res) {
+	res.redirect('/');
+});
+
+app.listen(3000, function done() {
+	console.log('Example site start at :3000');
+});
